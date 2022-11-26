9f4c888cf649aae932b4294bf051d16a10016d42
Le Kien Truc <truc@nops.io>
2022-11-25 16:20:57 -0800

    Add different pod metrics

---
+++ nops_k8s_agent/nops_k8s_agent/libs/pod_usage.py
@@ -0,0 +1,10 @@
+class PodUsage:
+    def get_pod_list(self) -> list:
+        # Expect retuning is a list of pod with pod name
+        return
+
+    def get_pod_usage_list(self) -> dict:
+        # What would this function do:
+        # Get list of pod
+        # Map all the metrics related to the pod
+        return
