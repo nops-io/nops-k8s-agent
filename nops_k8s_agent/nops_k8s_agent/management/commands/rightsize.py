5e3c09f42499c1e0618d80b2984bec2dd5ff1f8d
Egor Bak <egorbak@Egors-MacBook-Pro.local>
2024-04-12 21:45:56 +0400

    iterate deployments and patch pods

--- nops_k8s_agent/nops_k8s_agent/management/commands/rightsize.py
+++ nops_k8s_agent/nops_k8s_agent/management/commands/rightsize.py
@@ -1,11 +1,15 @@
 import asyncio
 from collections import defaultdict
+from typing import Any
 
 from django.core.management.base import BaseCommand
 
+from kubernetes.utils.quantity import parse_quantity
 from kubernetes_asyncio import client
 from kubernetes_asyncio import config
 from kubernetes_asyncio.client.api_client import ApiClient
+
+from nops_k8s_agent.rightsizing.utils import format_quantity
 
 
 class Command(BaseCommand):
@@ -13,26 +17,161 @@ class Command(BaseCommand):
     def _init_client():
         config.load_incluster_config()
 
-    @staticmethod
-    async def _list_pods():
-        pods = defaultdict(dict)
-
-        async with ApiClient() as api:
-            v1_client = client.CoreV1Api(api)
-            all_pods_response = await v1_client.list_pod_for_all_namespaces(watch=False)
-
-            for pod in all_pods_response.items:
-                pods[pod.metadata.namespace][pod.metadata.name] = {
-                    "containers": {container.name: container.resources for container in pod.spec.containers}
-                }
-
-        return pods
-
-    async def rightsize(self):
-        self._init_client()
-        pods = await self._list_pods()
-        print(pods)
-
     def handle(self, *args, **options):
         loop = asyncio.get_event_loop()
         return loop.run_until_complete(self.rightsize())
+
+    async def rightsize(self):
+        self._init_client()
+        tasks = []
+
+        async with ApiClient() as api:
+            v1_client = client.CoreV1Api(api)
+            all_namespaces = await v1_client.list_namespace(watch=False)
+            for namespace in all_namespaces.items:
+                tasks.append(self.process_namespace(namespace.metadata.name))
+
+        await asyncio.gather(*tasks)
+
+    async def process_namespace(self, namespace: str):
+        all_deployments = await self._list_deployments(namespace=namespace)
+        configured_deployments = all_deployments  # TODO: add filtering logic
+        configured_deployments = await self._enrich_deployments_with_pods(namespace, configured_deployments)
+        pods_to_patch = await self._find_pods_to_patch(namespace, configured_deployments)
+
+        tasks = []
+        for pod_name, pod_data in pods_to_patch.items():
+            tasks.append(self._patch_pod(namespace, pod_name, pod_data.get("containers", {})))
+
+        await asyncio.gather(*tasks)
+
+    @staticmethod
+    async def _list_deployments(namespace: str):
+        deployments = {}
+
+        async with ApiClient() as api:
+            apps_v1 = client.AppsV1Api(api)
+            all_deployments = await apps_v1.list_namespaced_deployment(namespace=namespace, watch=False)
+
+            for deployment in all_deployments.items:
+                selector = deployment.spec.selector.match_labels
+                deployments[deployment.metadata.name] = {"selector": selector}
+
+        return deployments
+
+    @staticmethod
+    async def _enrich_deployments_with_pods(namespace: str, deployments: dict[str, Any]) -> dict[str, Any]:
+        """
+        Enriches deployments with pods that are controlled by them.
+        """
+
+        async with ApiClient() as api:
+            v1_client = client.CoreV1Api(api)
+            for deployment, deployment_data in deployments.items():
+                # Fetch the selector for each deployment
+                selector = deployment_data.get("selector")
+                selector_query = ",".join([f"{k}={v}" for k, v in selector.items()])
+                deployment_pods = await v1_client.list_namespaced_pod(
+                    namespace=namespace, label_selector=selector_query
+                )
+                deployment_data["pods"] = {}
+                for pod in deployment_pods.items:
+                    deployment_data["pods"][pod.metadata.name] = {
+                        "containers": {container.name: container.resources for container in pod.spec.containers}
+                    }
+
+        return deployments
+
+    @staticmethod
+    async def _get_container_recommendations(namespace: str) -> dict[str, Any]:
+        """
+        MOCKED! Gets limits/requests recommendations for containers by deployment
+        Returns:
+        {
+            "deployment_1": {
+                "container_1": {
+                    "requests": {"cpu": 0.2, "memory": 220000000}
+                }
+            }
+        }
+        """
+        return {"opencost": {"opencost": {"requests": {"cpu": 0.2, "memory": 73400320}}}}
+
+    async def _find_pods_to_patch(self, namespace: str, configured_deployments: dict[str, Any]) -> dict[str, Any]:
+        pods_to_patch = defaultdict(lambda: defaultdict(dict))
+        recommendations = await self._get_container_recommendations(namespace=namespace)
+
+        for deployment, deployment_data in configured_deployments.items():
+            deployment_recommendations = recommendations.get(deployment, {})
+            if not deployment_recommendations:
+                continue
+
+            for pod, pod_data in deployment_data.get("pods", {}).items():
+                for container, container_data in pod_data.get("containers", {}).items():
+                    container_requests_recommendations = deployment_recommendations.get(deployment, {}).get(
+                        "requests", {}
+                    )
+                    if not container_requests_recommendations:
+                        continue
+
+                    new_requests = {}
+
+                    recommended_cpu_requests = container_requests_recommendations.get("cpu")
+                    recommended_ram_requests = container_requests_recommendations.get("memory")
+
+                    actual_cpu_requests = container_data.requests.get("cpu")
+                    actual_ram_requests = container_data.requests.get("memory")
+
+                    # Set new CPU requests if they were not set OR current request is lower than recommended
+                    if recommended_cpu_requests and not actual_cpu_requests:
+                        new_requests["cpu"] = recommended_cpu_requests
+                    elif recommended_cpu_requests and parse_quantity(actual_cpu_requests) < recommended_cpu_requests:
+                        new_requests["cpu"] = recommended_cpu_requests
+
+                    # Set new Memory requests if they were not set OR current request is lower than recommended
+                    if recommended_ram_requests and not actual_ram_requests:
+                        new_requests["memory"] = recommended_ram_requests
+                    elif recommended_ram_requests and parse_quantity(actual_ram_requests) < recommended_ram_requests:
+                        new_requests["memory"] = recommended_ram_requests
+
+                    if new_requests:
+                        pods_to_patch[pod]["containers"][container] = {"requests": {}}
+
+                        # Convert resources requests back from Decimal numbers to k8s notation (e.g. 256Mi)
+                        if new_requests.get("cpu"):
+                            pods_to_patch[pod]["containers"][container]["requests"]["cpu"] = format_quantity(
+                                new_requests["cpu"], "m"
+                            )
+                        if new_requests.get("memory"):
+                            pods_to_patch[pod]["containers"][container]["requests"]["memory"] = format_quantity(
+                                new_requests["memory"], "Mi"
+                            )
+
+        return pods_to_patch
+
+    @staticmethod
+    async def _patch_pod(namespace: str, pod_name: str, containers: dict[str, Any]):
+        try:
+            async with ApiClient() as api:
+                v1 = client.CoreV1Api(api)
+
+                pod_patch_body = {
+                    "spec": {
+                        "containers": [
+                            {"name": container_name, "resources": resources}
+                            for container_name, resources in containers.items()
+                        ]
+                    }
+                }
+
+                print(pod_patch_body)
+
+                print(f"Patching pod {pod_name} setting containers {containers}")
+                await v1.patch_namespaced_pod(
+                    pod_name,
+                    namespace,
+                    pod_patch_body,
+                    _content_type="application/json-patch+json",  # (optional, default if patch is a list)
+                )
+        except Exception as e:
+            print(f"Error: {e}")
---
+++ nops_k8s_agent/nops_k8s_agent/rightsizing/utils.py
@@ -0,0 +1,67 @@
+from decimal import Decimal
+
+_EXPONENTS = {
+    "n": -3,
+    "u": -2,
+    "m": -1,
+    "K": 1,
+    "k": 1,
+    "M": 2,
+    "G": 3,
+    "T": 4,
+    "P": 5,
+    "E": 6,
+}
+
+
+# Waits for https://github.com/kubernetes-client/python/issues/2205 to be merged
+def format_quantity(quantity_value, suffix, quantize=None) -> str:
+    """
+    Takes a decimal and produces a string value in kubernetes' canonical quantity form,
+    like "200Mi".Users can specify an additional decimal number to quantize the output.
+    Example -  Relatively increase pod memory limits:
+    # retrieve my_pod
+    current_memory: Decimal = parse_quantity(my_pod.spec.containers[0].resources.limits.memory)
+    desired_memory = current_memory * 1.2
+    desired_memory_str = format_quantity(desired_memory, suffix="Gi", quantize=Decimal(1))
+    # patch pod with desired_memory_str
+    'quantize=Decimal(1)' ensures that the result does not contain any fractional digits.
+    Supported SI suffixes:
+    base1024: Ki | Mi | Gi | Ti | Pi | Ei
+    base1000: n | u | m | "" | k | M | G | T | P | E
+    See https://github.com/kubernetes/apimachinery/blob/master/pkg/api/resource/quantity.go
+    Input:
+    quantity: Decimal.  Quantity as a number which is supposed to converted to a string
+                        with SI suffix.
+    suffix: string.     The desired suffix/unit-of-measure of the output string
+    quantize: Decimal.  Can be used to round/quantize the value before the string
+                        is returned. Defaults to None.
+    Returns:
+    string. Canonical Kubernetes quantity string containing the SI suffix.
+    Raises:
+    ValueError if the SI suffix is not supported.
+    """
+
+    if not suffix:
+        return str(quantity_value)
+
+    if isinstance(quantity_value, float) or isinstance(quantity_value, int):
+        quantity_value = Decimal(quantity_value)
+
+    if suffix.endswith("i"):
+        base = 1024
+    elif len(suffix) == 1:
+        base = 1000
+    else:
+        raise ValueError(f"{quantity_value} has unknown suffix")
+
+    if suffix == "ki":
+        raise ValueError(f"{quantity_value} has unknown suffix")
+
+    if suffix[0] not in _EXPONENTS:
+        raise ValueError(f"{quantity_value} has unknown suffix")
+
+    different_scale = quantity_value / Decimal(base ** _EXPONENTS[suffix[0]])
+    if quantize:
+        different_scale = different_scale.quantize(quantize)
+    return str(different_scale) + suffix
