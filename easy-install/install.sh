aafd3aaf45a2268673252840ed486b2c552107ea
Andre Leite <andre@nops.io>
2024-04-23 07:50:51 -0300

    Add more checks at beginning of script and point to links instead of local files.

--- easy-install/install.sh
+++ easy-install/install.sh
@@ -3,17 +3,36 @@
 # Exit script on error
 set -e
 
-# AWS credentials with write permission to S3 bucket
-# REPLACE THESE VALUES
-######################################################################
+# Use the access key id and secret access key with writing permission to the setup bucket
+####################  REPLACE THESE VALUES ############################
 AWS_ACCESS_KEY_ID="<REPLACE-YourAccessKeyId>"
 AWS_SECRET_ACCESS_KEY="<REPLACE-YourSecretAccessKey>"
-
 APP_NOPS_K8S_AGENT_CLUSTER_ARN="<REPLACE-YourClusternARN>"
+#######################################################################
 APP_AWS_S3_BUCKET="<REPLACE-YourS3Bucket>"
 APP_AWS_S3_PREFIX="<REPLACE-YourS3Prefix>"
-#######################################################################
-# DONT CHANGE ANYTHING BELOW THIS
+# Check if helm and kubectl are installed
+if ! command -v kubectl &>/dev/null; then
+    echo "kubectl is not installed. Please install kubectl and try again."
+    exit 1
+fi
+
+if ! command -v helm &>/dev/null; then
+    echo "Helm is not installed. Please install Helm and try again."
+    exit 1
+fi
+
+# Check current kubectl context and compare it with the required ARN
+current_context=$(kubectl config current-context)
+if [ "$current_context" != "$APP_NOPS_K8S_AGENT_CLUSTER_ARN" ]; then
+    echo "Current context ($current_context) is not set to required ARN ($APP_NOPS_K8S_AGENT_CLUSTER_ARN). Attempting to switch..."
+    if ! kubectl config use-context "$APP_NOPS_K8S_AGENT_CLUSTER_ARN"; then
+        echo "Failed to switch to context $APP_NOPS_K8S_AGENT_CLUSTER_ARN. Please check your configuration."
+        exit 1
+    fi
+    echo "Switched context to $APP_NOPS_K8S_AGENT_CLUSTER_ARN successfully."
+fi
+
 # Ensure AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are replaced
 if [[ $AWS_ACCESS_KEY_ID == "<REPLACE-YourAccessKeyId>" || $AWS_SECRET_ACCESS_KEY == "<REPLACE-YourSecretAccessKey>" ]]; then
   echo "AWS credentials must be set before running this script."
@@ -44,7 +63,6 @@ fi
 # Set kubectl context to use nops-k8s-agent namespace
 kubectl config set-context --current --namespace=$nops_k8s_agent_namespace || { echo "Failed to set kubectl context to nops-k8s-agent namespace"; exit 1; }
 
-
 # Check if the secret already exists
 nops_k8s_agent_secret="nops-k8s-agent"
 if kubectl get secret $nops_k8s_agent_secret --namespace $nops_k8s_agent_namespace >/dev/null 2>&1; then
@@ -68,15 +86,15 @@ else
     fi
 fi
 # Installing nops-Prometheus
-helm upgrade --install nops-prometheus prometheus --repo https://prometheus-community.github.io/helm-charts  --namespace nops-prometheus-system --create-namespace   -f ./prometheus.yaml || { echo "Failed to install Prometheus"; exit 1; }
+helm upgrade --install nops-prometheus prometheus --repo https://prometheus-community.github.io/helm-charts  --namespace nops-prometheus-system --create-namespace   -f https://raw.githubusercontent.com/nops-io/nops-k8s-agent/master/easy-install/prometheus.yaml || { echo "Failed to install Prometheus"; exit 1; }
 
 # Installing nops-cost
 helm upgrade -i nops-cost --repo https://opencost.github.io/opencost-helm-chart opencost \
---namespace nops-cost --create-namespace -f ./nops-cost.yaml || { echo "Failed to install nops-cost"; exit 1; }
+--namespace nops-cost --create-namespace -f https://raw.githubusercontent.com/nops-io/nops-k8s-agent/master/easy-install/nops-cost.yaml || { echo "Failed to install nops-cost"; exit 1; }
 
 # Installing k8s-agent
 helm upgrade -i nops-k8s-agent --repo https://nops-io.github.io/nops-k8s-agent \
-nops-k8s-agent --namespace nops-k8s-agent -f ./values.yaml \
+nops-k8s-agent --namespace nops-k8s-agent -f https://raw.githubusercontent.com/nops-io/nops-k8s-agent/master/easy-install/values.yaml \
 --set env_variables.APP_NOPS_K8S_AGENT_CLUSTER_ARN=$APP_NOPS_K8S_AGENT_CLUSTER_ARN,\
 env_variables.APP_AWS_S3_BUCKET=$APP_AWS_S3_BUCKET,\
 env_variables.APP_AWS_S3_PREFIX=$APP_AWS_S3_PREFIX || { echo "Failed to install k8s-agent"; exit 1; }
