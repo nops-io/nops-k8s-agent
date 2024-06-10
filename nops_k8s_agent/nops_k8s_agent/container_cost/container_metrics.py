2a62a31fc29b6555ce61a282a3fc16a70b8bcb1f
Marcos Heidemann <marcos@nops.io>
2024-06-10 14:12:54 -0300

    Trying to ajust the agent to collect raw metrics from prometheus

--- nops_k8s_agent/nops_k8s_agent/container_cost/container_metrics.py
+++ nops_k8s_agent/nops_k8s_agent/container_cost/container_metrics.py
@@ -20,18 +20,9 @@ class ContainerMetrics(BaseMetrics):
         "container_fs_usage_bytes": ["namespace", "pod", "container"],
         "container_fs_reads_bytes_total": ["namespace", "pod", "container"],
         "container_fs_writes_bytes_total": ["namespace", "pod", "container"], 
-        "rate_container_cpu_usage_seconds_total": [],
     }
     FILE_PREFIX = "container_metrics"
     FILENAME = f"v{SCHEMA_VERSION_DATE}_container_metrics_0-{derive_suffix_from_settings()}.parquet"
 
     def get_metrics(self, start_time: datetime, end_time: datetime, metric_name: str, step: str) -> Any:
-        if metric_name == "rate_container_cpu_usage_seconds_total":
-            query = 'rate(container_cpu_usage_seconds_total{pod!="",container!=""}[5m])'
-            try:
-                response = self.prom_client.custom_query_range(query, start_time=start_time, end_time=end_time, step=step)
-                return response
-            except Exception as e:
-                logger.error(f"Error in get_metrics: {e}")
-                return None
         return super().get_metrics(start_time, end_time, metric_name, step)
