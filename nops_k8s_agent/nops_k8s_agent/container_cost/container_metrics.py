43cc47b6260ce77ebb654e2c00e8231a3100d873
hayk <hharut234@gmail.com>
2024-06-17 17:39:42 +0400

    add non-groupby metrics

--- nops_k8s_agent/nops_k8s_agent/container_cost/container_metrics.py
+++ nops_k8s_agent/nops_k8s_agent/container_cost/container_metrics.py
@@ -13,25 +13,37 @@ class ContainerMetrics(BaseMetrics):
     list_of_metrics = {
         "container_cpu_usage_seconds_total": ["namespace", "pod", "container"],
         "container_cpu_cfs_throttled_seconds_total": ["namespace", "pod", "container"],
-        "container_memory_usage_bytes": ["namespace", "pod", "container"], 
+        "container_memory_usage_bytes": ["namespace", "pod", "container"],
         "container_memory_working_set_bytes": ["namespace", "pod", "container"],
         "container_memory_cache": ["namespace", "pod", "container"],
         "container_memory_rss": ["namespace", "pod", "container"],
         "container_fs_usage_bytes": ["namespace", "pod", "container"],
         "container_fs_reads_bytes_total": ["namespace", "pod", "container"],
-        "container_fs_writes_bytes_total": ["namespace", "pod", "container"], 
-        "rate_container_cpu_usage_seconds_total": [],
+        "container_fs_writes_bytes_total": ["namespace", "pod", "container"],
     }
     FILE_PREFIX = "container_metrics"
     FILENAME = f"v{SCHEMA_VERSION_DATE}_container_metrics_0-{derive_suffix_from_settings()}.parquet"
 
-    def get_metrics(self, start_time: datetime, end_time: datetime, metric_name: str, step: str) -> Any:
-        if metric_name == "rate_container_cpu_usage_seconds_total":
-            query = 'rate(container_cpu_usage_seconds_total{pod!="",container!=""}[5m])'
-            try:
-                response = self.prom_client.custom_query_range(query, start_time=start_time, end_time=end_time, step=step)
-                return response
-            except Exception as e:
-                logger.error(f"Error in get_metrics: {e}")
-                return None
-        return super().get_metrics(start_time, end_time, metric_name, step)
+class ContainerMetricsGranular(BaseMetrics):
+    list_of_metrics = {
+        "container_memory_usage_bytes": [],
+        "container_cpu_usage_seconds_total": [],
+        "container_memory_rss": [],
+        "container_memory_cache": [],
+        "container_memory_working_set_bytes": [],
+        "container_cpu_cfs_throttled_seconds_total": [],
+        "container_spec_cpu_quota": [],
+        "container_spec_cpu_period": [],
+    }
+    FILE_PREFIX = "container_metrics_granular"
+    FILENAME = f"v{SCHEMA_VERSION_DATE}_container_metrics_granular_0-{derive_suffix_from_settings()}.parquet"
+
+    def get_metrics(self, metric_name: str, **kwargs) -> Any:
+        query = f"{metric_name}[60m]"
+        try:
+            response = self.prom_client.custom_query(query)
+            logger.info(f"{metric_name} response: {response}")
+            return response
+        except Exception as e:
+            logger.error(f"Error in get_metrics: {e}")
+        return None
